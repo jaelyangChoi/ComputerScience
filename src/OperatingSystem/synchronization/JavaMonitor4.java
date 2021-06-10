a6107f690a0074225746498f9dd30b9bd82282cc
Jealyang <jaelyangchoi@gmail.com>
2021-06-10 20:09:29 +0900

    study: mutual exclusion 성립

---
+++ src/OperatingSystem/synchronization/JavaMonitor4.java
@@ -0,0 +1,42 @@
+package OperatingSystem.synchronization;
+/*
+ * 상호배제 성립
+ */
+
+public class JavaMonitor4 {
+    static class Counter {
+        public static int count = 0; //공유 데이터
+
+        public void increment() {
+            synchronized (this) { //모니터 락을 this로부터 얻는다. this가 다르면 동기화x
+                count++;
+            }
+        }
+    }
+
+    static class MyRunnable implements Runnable {
+        private Counter counter;
+
+        public MyRunnable(Counter counter) {
+            this.counter = counter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++)
+                counter.increment();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Thread[] threads = new Thread[5];
+        Counter counter = new Counter();
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new MyRunnable(counter)); //모니터 락 공유
+            threads[i].start();
+        }
+        for (int i = 0; i < threads.length; i++)
+            threads[i].join();
+        System.out.println("counter = " + Counter.count);
+    }
+}
